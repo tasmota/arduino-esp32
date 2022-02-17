0a45844d453d2e9e0401bd9a14799d9e4ee2aad2
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2022-02-17 11:16:40 +0100

    Update HardwareSerial.h

--- cores/esp32/HardwareSerial.h
+++ cores/esp32/HardwareSerial.h
@@ -107,7 +107,13 @@ public:
     void setDebugOutput(bool);
     
     void setRxInvert(bool);
-    void setPins(uint8_t rxPin, uint8_t txPin);
+
+    // Negative Pin Number will keep it unmodified, thus this function can set individual pins
+    // SetPins shall be called after Serial begin()
+    void setPins(int8_t rxPin, int8_t txPin, int8_t ctsPin = -1, int8_t rtsPin = -1);
+    // Enables or disables Hardware Flow Control using RTS and/or CTS pins (must use setAllPins() before)
+    void setHwFlowCtrlMode(uint8_t mode = HW_FLOWCTRL_CTS_RTS, uint8_t threshold = 64);   // 64 is half FIFO Length
+
     size_t setRxBufferSize(size_t new_size);
 
 protected:
@@ -123,10 +129,10 @@ extern void serialEventRun(void) __attribute__((weak));
 #define ARDUINO_USB_CDC_ON_BOOT 0
 #endif
 #if ARDUINO_USB_CDC_ON_BOOT //Serial used for USB CDC
-#if !ARDUINO_USB_MODE
 #include "USB.h"
 #include "USBCDC.h"
-#endif
+extern HardwareSerial Serial0;
+#elif ARDUINO_HW_CDC_ON_BOOT
 extern HardwareSerial Serial0;
 #else
 extern HardwareSerial Serial;
