abdd82b7f2d1b6ba7f18f00d6b681e02d9259a60
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2023-02-06 14:01:33 +0100

    Update install-platformio-esp32.sh

--- .github/scripts/install-platformio-esp32.sh
+++ .github/scripts/install-platformio-esp32.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export PLATFORMIO_ESP32_PATH="$HOME/.platformio/packages/framework-arduinoespressif32"
-PLATFORMIO_ESP32_URL="https://github.com/tasmota/platform-espressif32.git"
+PLATFORMIO_ESP32_URL="https://github.com/platformio/platform-espressif32.git"
 
 TOOLCHAIN_VERSION="8.4.0+2021r2-patch5"
 ESPTOOLPY_VERSION="1.40400.0"
