b86db3c0ddd44ecd2a96d81fb173aa26f983ee7b
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2023-07-24 11:04:53 +0200

    Update pins_arduino.h

--- variants/esp32c2/pins_arduino.h
+++ variants/esp32c2/pins_arduino.h
@@ -8,6 +8,10 @@
 #define NUM_DIGITAL_PINS        14
 #define NUM_ANALOG_INPUTS       5
 
+static const uint8_t LED_BUILTIN = SOC_GPIO_PIN_COUNT-13;
+#define BUILTIN_LED  LED_BUILTIN // backward compatibility
+#define LED_BUILTIN LED_BUILTIN
+
 #define analogInputToDigitalPin(p)  (((p)<NUM_ANALOG_INPUTS)?(analogChannelToDigitalPin(p)):-1)
 #define digitalPinToInterrupt(p)    (((p)<NUM_DIGITAL_PINS)?(p):-1)
 #define digitalPinHasPWM(p)         (p < EXTERNAL_NUM_INTERRUPTS)
