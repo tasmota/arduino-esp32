4a0b18eec9418f044b1ada7c703692dcea5c4e45
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2023-10-06 14:26:14 +0200

    fic c2 pins_arduino.h

--- variants/esp32c2/pins_arduino.h
+++ variants/esp32c2/pins_arduino.h
@@ -4,17 +4,10 @@
 #include <stdint.h>
 #include "soc/soc_caps.h"
 
-#define EXTERNAL_NUM_INTERRUPTS 14
-#define NUM_DIGITAL_PINS        14
-#define NUM_ANALOG_INPUTS       5
 
 static const uint8_t LED_BUILTIN = SOC_GPIO_PIN_COUNT-13;
 #define BUILTIN_LED  LED_BUILTIN // backward compatibility
-#define LED_BUILTIN LED_BUILTIN
-
-#define analogInputToDigitalPin(p)  (((p)<NUM_ANALOG_INPUTS)?(analogChannelToDigitalPin(p)):-1)
-#define digitalPinToInterrupt(p)    (((p)<NUM_DIGITAL_PINS)?(p):-1)
-#define digitalPinHasPWM(p)         (p < EXTERNAL_NUM_INTERRUPTS)
+#define LED_BUILTIN LED_BUILTIN  // allow testing #ifdef LED_BUILTIN
 
 static const uint8_t TX = 20;
 static const uint8_t RX = 19;
