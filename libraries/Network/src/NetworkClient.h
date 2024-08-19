5b03689463cbfde7ac7ca95576be3343a0377cae
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2024-08-19 21:55:53 +0200

    Tasmota changes

--- libraries/Network/src/NetworkClient.h
+++ libraries/Network/src/NetworkClient.h
@@ -54,7 +54,6 @@ public:
   size_t write(const uint8_t *buf, size_t size);
   size_t write_P(PGM_P buf, size_t size);
   size_t write(Stream &stream);
-  void flush();  // Print::flush tx
   int available();
   int read();
   int read(uint8_t *buf, size_t size);
@@ -63,7 +62,7 @@ public:
     return readBytes((char *)buffer, length);
   }
   int peek();
-  void clear();  // clear rx
+  void flush();
   void stop();
   uint8_t connected();
   void setSSE(bool sse);
