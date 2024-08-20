c87e66ac98d271412c680bb10a3f210bb75b252c
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2024-08-20 16:45:54 +0200

    no support yet for Touch v3

--- cores/esp32/esp32-hal-touch.c
+++ cores/esp32/esp32-hal-touch.c
@@ -12,9 +12,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 #include "soc/soc_caps.h"
-#if CONFIG_IDF_TARGET_ESP32P4
-#else
+
 #if SOC_TOUCH_SENSOR_SUPPORTED
+#if SOC_TOUCH_SENSOR_VERSION == 3  // ESP32P4
+#else
 #include "driver/touch_sensor.h"
 #include "esp32-hal-touch.h"
 #include "esp32-hal-periman.h"
@@ -325,5 +326,5 @@ extern void touchAttachInterruptArg(uint8_t, voidArgFuncPtr, void *, touch_value
 extern void touchDetachInterrupt(uint8_t) __attribute__((weak, alias("__touchDettachInterrupt")));
 extern void touchSetCycles(uint16_t, uint16_t) __attribute__((weak, alias("__touchSetCycles")));
 
+#endif /* SOC_TOUCH_SENSOR_VERSION == 3 */
 #endif /* SOC_TOUCH_SENSOR_SUPPORTED */
-#endif /* !defined(CONFIG_IDF_TARGET_ESP32P4) */
