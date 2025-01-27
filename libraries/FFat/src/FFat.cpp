e38a51b39d458eadd5a8db060d1ad3923f277fb1
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2025-01-28 00:32:45 +0100

    no `use_one_fat`

--- libraries/FFat/src/FFat.cpp
+++ libraries/FFat/src/FFat.cpp
@@ -49,8 +49,7 @@ bool F_Fat::begin(bool formatOnFail, const char *basePath, uint8_t maxOpenFiles,
     .format_if_mount_failed = formatOnFail,
     .max_files = maxOpenFiles,
     .allocation_unit_size = CONFIG_WL_SECTOR_SIZE,
-    .disk_status_check_enable = false,
-    .use_one_fat = false
+    .disk_status_check_enable = false
   };
   esp_err_t err = esp_vfs_fat_spiflash_mount_rw_wl(basePath, partitionLabel, &conf, &_wl_handle);
   if (err) {
@@ -102,7 +101,7 @@ bool F_Fat::format(bool full_wipe, char *partitionLabel) {
   }
   // Now do a mount with format_if_fail (which it will)
   esp_vfs_fat_mount_config_t conf = {
-    .format_if_mount_failed = true, .max_files = 1, .allocation_unit_size = CONFIG_WL_SECTOR_SIZE, .disk_status_check_enable = false, .use_one_fat = false
+    .format_if_mount_failed = true, .max_files = 1, .allocation_unit_size = CONFIG_WL_SECTOR_SIZE, .disk_status_check_enable = false
   };
   result = esp_vfs_fat_spiflash_mount_rw_wl("/format_ffat", partitionLabel, &conf, &temp_handle);
   esp_vfs_fat_spiflash_unmount_rw_wl("/format_ffat", temp_handle);
