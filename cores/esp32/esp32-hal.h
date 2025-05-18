a4012fc79244c3d72312141af5c6c8f732ea0113
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2025-05-18 11:23:53 +0200

    Update esp32-hal.h

--- cores/esp32/esp32-hal.h
+++ cores/esp32/esp32-hal.h
@@ -96,6 +96,7 @@ void yield(void);
 #include "esp32-hal-rmt.h"
 #include "esp32-hal-sigmadelta.h"
 #include "esp32-hal-timer.h"
+#include "esp32-hal-bt.h"
 #include "esp32-hal-psram.h"
 #include "esp32-hal-rgb-led.h"
 #include "esp32-hal-cpu.h"
