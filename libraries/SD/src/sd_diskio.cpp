3f99d663f0b6aca246cd0f4f41366544bcc5a3bd
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2026-03-24 00:12:19 +0100

    Refactor esp_vfs_fat_register call parameters

--- libraries/SD/src/sd_diskio.cpp
+++ libraries/SD/src/sd_diskio.cpp
@@ -796,12 +796,7 @@ bool sdcard_mount(uint8_t pdrv, const char *path, uint8_t max_files, bool format
 
   FATFS *fs;
   char drv[3] = {(char)('0' + pdrv), ':', 0};
-  esp_vfs_fat_conf_t conf = {
-    .base_path = path,
-    .fat_drive = drv,
-    .max_files = (size_t)max_files,
-  };
-  esp_err_t err = esp_vfs_fat_register(&conf, &fs);
+  esp_err_t err = esp_vfs_fat_register(path, drv, max_files, &fs);
   if (err == ESP_ERR_INVALID_STATE) {
     log_e("esp_vfs_fat_register failed 0x(%x): SD is registered.", err);
     return false;
